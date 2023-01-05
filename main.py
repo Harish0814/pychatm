eff2e99302935acb2d23e92a85a5209af6ab1e18
Harish0814 <122007275+Harish0814@users.noreply.github.com>
2023-01-05 14:54:54 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('my name is Arunkumar sriharish muthumalai')
+    print('I am 18 years old')
+    print('I am from india')
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
